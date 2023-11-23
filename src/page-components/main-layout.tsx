4705a0b8e4b21c22b9d34718dcf6b8ed4f261214
Dalveer Singh <dalveersidhu97@gmail.com>
2023-11-22 23:15:22 -0700

    changed height from 100vh to 100dvh for main layout container to fix mobile height issue

--- src/page-components/main-layout.tsx
+++ src/page-components/main-layout.tsx
@@ -7,13 +7,12 @@ import TopBar from './top-bar';
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
 
     return (
-        <div className='w-[100vw] h-[100vh] overflow-hidden lg:p-16 relative'>
+        <div className='w-full h-[100dvh] overflow-hidden lg:p-16 relative'>
             <div className='overflow-auto h-full w-full max-w-[90rem] lg:m-auto flex items-start lg:gap-4 lg:p-4'>
                 <SideBarPC></SideBarPC>
                 <div className='flex flex-col w-full h-full overflow-auto lg:shadow-[0px_0px_5px_rgba(0,0,0,.2)] lg:rounded-lg'>
                     <TopBar></TopBar>
                     <div className='flex flex-col w-full h-full overflow-auto lg:relative'>
-                        {/* <div className='absolute top-0 left-0 bg-[rgba(0,0,0,.25)] w-full h-full z-[500]'></div> */}
                         <MainNav></MainNav>
                         <div className='flex-grow overflow-auto'>
                             {children}
